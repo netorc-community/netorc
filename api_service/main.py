f1be8c15614a62d76400d270593da7f9c60d8899
Antonió <antonio@tomosev.dev>
2023-05-07 15:37:26 +0100

    added temp example task

--- api_service/main.py
+++ api_service/main.py
@@ -5,11 +5,15 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from miscellaneous.addons.exceptions import TMFException
+from miscellaneous.addons.decorators import queue_task
 
-from api_service.tmforum import tmf653
+from api_service.tmforum import tmf_653
+
+from worker_service.tasks.example import example_task
+
 
 fastapi = FastAPI()
-fastapi.include_router(tmf653.router)  # Service Test Management
+fastapi.include_router(tmf_653.router)  # Service Test Management
 
 
 @fastapi.exception_handler(TMFException)
@@ -40,3 +44,9 @@ async def tmf_exception_handler(request: Request, exc: TMFException):
         status_code=exc.status_code,
         content={"code": exc.code, "reason": exc.reason},
     )
+
+
+@fastapi.get("/api")
+async def example() -> list:
+    task = queue_task(example_task)
+    return [{"id": task.id}]
