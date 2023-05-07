e4887d03ab94c609808bb43e1f52be68110ada40
Antonió <antonio@tomosev.dev>
2023-05-07 17:29:36 +0100

    removed async engine

--- miscellaneous/database/db.py
+++ miscellaneous/database/db.py
@@ -1,12 +1,12 @@
 """
 db.py
 """
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from settings import settings
 
-engine = create_async_engine(settings.database)
+engine = create_engine(settings.database)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
