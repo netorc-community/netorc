54f32236939343c2a2091c975a77fc9caf6e5c09
Antonió <antonio@tomosev.dev>
2023-05-08 12:32:09 +0100

    using sqlmodel

--- miscellaneous/database/db.py
+++ miscellaneous/database/db.py
@@ -1,13 +1,7 @@
 """
 db.py
 """
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine
 from settings import settings
 
-engine = create_engine(settings.database)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+engine = create_engine(settings.database, echo=True)
