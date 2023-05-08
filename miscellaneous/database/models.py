92de41668a0a3efc57cca63e791520ce9dad6e9e
Antonió <antonio@tomosev.dev>
2023-05-08 12:32:22 +0100

    work in progress

--- miscellaneous/database/models.py
+++ miscellaneous/database/models.py
@@ -1,8 +1,20 @@
 """
 models.py
 """
-from miscellaneous.database.db import Base
+from datetime import datetime
+from typing import Optional
+from sqlmodel import Field, SQLModel
 
+class UserModel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    firstname: Optional[str]
+    lastname: Optional[str]
+    email: str
+    username: str
+    password: str
+    password_salt: str
+    api_key: str
+    api_key_salt: str
+    created: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+    last_updated: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
-class User(Base):
-    __tablename__ = "users"
