acb0d2f369019ce4bfa53a2cabad85e1485e53f8
Antonió <antonio@tomosev.dev>
2023-05-08 20:51:41 +0100

    testing

--- manage.py
+++ manage.py
@@ -1,23 +1,24 @@
 """
 manage.py
 """
+from datetime import datetime
 import sys
 from sqlmodel import SQLModel, Session
 from miscellaneous.database.db import engine
-from miscellaneous.database.models import UserModel
+from miscellaneous.database.models import User, Service, ServiceType
 
-TestUser = UserModel(firstname="antonio", lastname="faria", email="antonio@something.com", username="antonio",
-                     password="password123", password_salt="salt", api_key="api-key-1", api_key_salt="salt")
+TestService = Service(id="GN001", name="test", description="test", service_type="test", category="test", state="up",
+                      start_date=datetime.now(), end_date=datetime.now())
 
 
 def main():
     if "migrate" in sys.argv:
         SQLModel.metadata.create_all(engine)
 
-    with Session(engine) as SessionLocal:
-        SessionLocal.add(TestUser)
-        SessionLocal.commit()
-        SessionLocal.close()
+    # with Session(engine) as SessionLocal:
+    #     SessionLocal.add(TestService)
+    #     SessionLocal.commit()
+    #     SessionLocal.close()
 
 
 if __name__ == "__main__":
