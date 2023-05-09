7666c62f2217db3539d5c454238a4b12fc1f40b3
Antonió <antonio@tomosev.dev>
2023-05-09 19:07:05 +0100

    wip manage.py

--- manage.py
+++ manage.py
@@ -1,25 +1,26 @@
 """
 manage.py
 """
-from datetime import datetime
 import sys
-from sqlmodel import SQLModel, Session
+from sqlmodel import SQLModel
 from miscellaneous.database.db import engine
-from miscellaneous.database.models import User, Service, ServiceType
-
-TestService = Service(id="GN001", name="test", description="test", service_type="test", category="test", state="up",
-                      start_date=datetime.now(), end_date=datetime.now())
+from miscellaneous.database import tables
 
 
 def main():
     if "migrate" in sys.argv:
-        SQLModel.metadata.create_all(engine)
-
-    # with Session(engine) as SessionLocal:
-    #     SessionLocal.add(TestService)
-    #     SessionLocal.commit()
-    #     SessionLocal.close()
+        SQLModel.metadata.create_all(engine)  # Creates all the tables in the database
 
 
 if __name__ == "__main__":
     main()
+
+
+# with Session(engine) as SessionLocal:
+#     SessionLocal.add(TestService)
+#     SessionLocal.commit()
+#     SessionLocal.close()
+
+#
+# TestService = Service(id="GN001", name="test", description="test", service_type="test", category="test", state="up",
+#                       start_date=datetime.now(), end_date=datetime.now())
