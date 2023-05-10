ce6024fb692fbf328d93dd5f9b20d8fe7f194f9c
Antonió <antonio@tomosev.dev>
2023-05-10 12:45:02 +0100

    fixes #12

--- api_service/headers.py
+++ api_service/headers.py
@@ -1,10 +1,11 @@
-from passlib.hash import pbkdf2_sha256
 from typing import Any
+from passlib.hash import pbkdf2_sha256
+from sqlmodel import Session, select
 from fastapi import Response, Depends, Security
 from fastapi.security.api_key import APIKeyHeader
+
 from settings import settings
-from sqlmodel import Session, select
-from miscellaneous.database.db import engine
+from miscellaneous.database.db import get_session
 from miscellaneous.database.tables import User
 from miscellaneous.addons.exceptions import APIException
 
@@ -18,18 +19,26 @@ require_general_http_headers = Depends(general_http_headers)
 api_key_header = APIKeyHeader(name=settings.api_key_header, auto_error=False)
 
 
-# TODO shared dependency for database session.
+def general_authentication_header(api_key: str = Security(api_key_header),
+                                  session: Session = Depends(get_session)) -> Any:
+    """
+    Retrieves api key in request header and checks api key exists in user database.
 
-async def general_authentication_header(api_key: str = Security(api_key_header)) -> Any:
+    Args:
+        api_key: request api key
+        session: database session dependency.
+
+    Raises:
+        APIException: 401 and 500 status codes.
+    """
     try:
-        with Session(engine) as session:
-            query = select(User)
-            result = session.exec(query).all()
-            check = [x.api_key for x in result if
-                     x.api_key.startswith("$pbkdf2-sha256") and pbkdf2_sha256.verify(api_key, x.api_key)]
-            # Delay is caused if key is not in the pbkdf2 format passlib expects.
-            if not check:
-                raise APIException(status_code=401, code="tbd", reason="tbd")
+        query = select(User)
+        result = session.exec(query).all()
+        check = [x.api_key for x in result if
+                 x.api_key.startswith("$pbkdf2-sha256") and pbkdf2_sha256.verify(api_key, x.api_key)]
+        # Delay is caused if key is not in the pbkdf2 format passlib expects.
+        if not check:
+            raise APIException(status_code=401, code="tbd", reason="tbd")
     except Exception as exc:
         raise APIException(status_code=500, code="tbd", reason="tbd") from exc
 
