fbe11b24d81e6ae85447d88820fe3bb9e1c34543
Antonió <antonio@tomosev.dev>
2023-05-20 20:01:57 +0100

    added password and key validation

---
+++ core/security/password.py
@@ -0,0 +1,16 @@
+def password_enforcer(password: str):
+    special_characters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+    if len(password) <= 8:
+        return False
+
+    if not any(x.isupper() for x in password):
+        return False
+
+    if not any(x.isdigit() for x in password):
+        return False
+
+    if not any(x in special_characters for x in password):
+        return False
+
+    return True
--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 """
 manage.py
 """
+import argparse
 import getpass
-import sys
 
 from passlib.hash import pbkdf2_sha256
 from sqlmodel import SQLModel
@@ -10,31 +10,49 @@ from sqlmodel import SQLModel
 from core import db
 from core.db import tables
 from core.metrics.logging import logger
+from core.security.password import password_enforcer
 
 
 def main(session=next(db.get_session())):
-    if "migrate" in sys.argv[1]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", choices=["migrate", "createsuperuser"])
+    args = parser.parse_args()
+
+    if args.action == "migrate":
         SQLModel.metadata.create_all(db.engine)
+        logger.info("Successfully migrated tables to database")
 
-    if "createsuperuser" in sys.argv[1]:
-        try:
-            superuser = tables.User()
-            superuser.username = input("username: ")
+    if args.action == "createsuperuser":
+        superuser = tables.User()
+        superuser.username = input("username: ")
 
+        while True:
             password = getpass.getpass(prompt="password: ")
-            superuser.password = pbkdf2_sha256.hash(password)
+            if not password_enforcer(password):
+                print("Password does not meet security requirements, please try again.")
+            else:
+                break
 
+        superuser.password = pbkdf2_sha256.hash(password)
+
+        while True:
             api_key = getpass.getpass(prompt="api key: ")
-            superuser.api_key = pbkdf2_sha256.hash(api_key)
+            if not password_enforcer(api_key):
+                print("API key does not meet security requirements, please try again.")
+            else:
+                break
 
+        superuser.api_key = pbkdf2_sha256.hash(api_key)
+
+        try:
             session.add(superuser)
             session.commit()
 
-            print(f"Superuser: {superuser.username}, created")
-            logger.info("Superuser: %s, created", superuser.username)
-
         except Exception as exc:
-            raise exc
+            logger.error("Error creating superuser: %s", exc)
+            raise
+
+        logger.info("Superuser: %s, created", superuser.username)
 
 
 if __name__ == "__main__":
