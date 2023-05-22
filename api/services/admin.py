869eb0be634c066bb9acb384ce78c50291d3300a
Antonió <antonio@tomosev.dev>
2023-05-22 22:20:29 +0100

    added admin.py

---
+++ api/services/admin.py
@@ -0,0 +1,61 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlmodel import Session, select
+
+from core import db
+from core.addons.exceptions import APIError
+from core.db.models import UserRead, UserCreate
+from core.db.tables import User
+
+router = APIRouter(prefix="/api/admin/v1", tags=["Administration"])
+
+
+@router.get("/users", response_model=List[UserRead])
+def get_users(username: str = None, session: Session = Depends(db.get_session)) -> list:
+    """
+    Returns a list of user/'s from the db.
+    Arguments can be included in the uri to be added to the db query.
+
+    Args:
+        username: an optional username filter.
+        session: db session dependency.
+
+    Returns:
+        A list of dictionary/s containing attributes for a user/s.
+
+    Raises:
+        APIException: 500 status code.
+    """
+    try:
+        if username is not None:
+            user = session.get(User, username)
+            if not user:
+                raise APIError(status_code=404, code="tbd", reason="tbd")
+
+            return [user]
+
+        query = select(User)
+        result = session.exec(query)
+        return [x for x in result]
+
+    except APIError:
+        raise
+
+    except Exception as exc:
+        raise APIError(status_code=500, code="tbd", reason="tbd") from exc
+
+
+@router.post("/users", )
+def post_users(user_create: UserCreate):
+    pass
+
+
+@router.patch("/users")
+def patch_users():
+    pass
+
+
+@router.delete("/users")
+def delete_users():
+    pass
