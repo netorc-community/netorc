c3a1a385d4ecd816123b4afba01c9fdd0375a456
Antonió <antonio@tomosev.dev>
2023-05-23 19:50:21 +0100

    cleared tests

--- tests/core/test_db.py
+++
@@ -1,6 +0,0 @@
-from core import db
-
-
-def test_db_connection():
-    _engine = db.engine
-    _engine.connect()
--- tests/core/test_redis.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-from settings import settings
-
-
-def test_redis_connection():
-    conn = redis.from_url(settings.redis)
-    conn.ping()
