21337f33a02cdd480238906a52c06a4081bfa96d
Antonió <antonio@tomosev.dev>
2023-05-24 15:28:00 +0100

    fixed user.py

--- core/security/user.py
+++ core/security/user.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from passlib.hash import pbkdf2_sha256
 
 from core import db
@@ -5,7 +7,7 @@ from core.db.tables import User
 from core.metrics.logging import logger
 
 
-def create_user(user: User, session=next(db.get_session())):
+def create_user(user: User, session: Callable = db.get_session):
     """
     Creates a new database user based on User object.
     Hashes user password and api key using SHA256.
@@ -15,18 +17,13 @@ def create_user(user: User, session=next(db.get_session())):
         session: database session
 
     Returns:
-        bool: True
+        user: database user object
     """
     user.password = pbkdf2_sha256.hash(user.password)
     user.api_key = pbkdf2_sha256.hash(user.api_key)
 
-    try:
-        session.add(user)
-        session.commit()
-
-    except Exception as exc:
-        logger.error("Error creating superuser")
-        raise
-
-    logger.info("User: %s, created", user.username)
-    return True
+    with session() as _session:
+        _session.add(user)
+        _session.commit()
+        logger.info("User: %s, created", user.username)
+        return user
