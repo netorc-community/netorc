cac9af810364a93d7eecfbcb8f1eb7ff06a95e66
Antonió <antonio@tomosev.dev>
2023-05-24 15:26:49 +0100

    added  post_user

--- api/services/admin.py
+++ api/services/admin.py
@@ -7,6 +7,7 @@ from core import db
 from core.addons.exceptions import APIError
 from core.db.models import UserRead, UserCreate
 from core.db.tables import User
+from core.security.user import create_user
 
 router = APIRouter(prefix="/api/admin/v1", tags=["Administration"])
 
@@ -28,16 +29,19 @@ def get_users(username: str = None, session: Session = Depends(db.get_session)) 
         APIException: 500 status code.
     """
     try:
-        if username is not None:
-            user = session.get(User, username)
-            if not user:
-                raise APIError(status_code=404, code="tbd", reason="tbd")
 
-            return [user]
+        if username is not None:
+            with session as _session:
+                user = _session.get(User, username)
+                if not user:
+                    raise APIError(status_code=404, code="tbd", reason="tbd")
+
+                return [user]
 
         query = select(User)
-        result = session.exec(query)
-        return [x for x in result]
+        with session as _session:
+            result = _session.exec(query)
+            return [x for x in result]
 
     except APIError:
         raise
@@ -46,9 +50,19 @@ def get_users(username: str = None, session: Session = Depends(db.get_session)) 
         raise APIError(status_code=500, code="tbd", reason="tbd") from exc
 
 
-@router.post("/users", )
+@router.post("/users", response_model=UserRead)
 def post_users(user_create: UserCreate):
-    pass
+    user = User()
+    user_data = user_create.dict(exclude_unset=True)
+    for k, v in user_data.items():
+        setattr(user, k, v)
+
+    try:
+        db_user = create_user(user)
+        return db_user
+
+    except Exception as exc:
+        raise APIError(status_code=500, code="tbd", reason="tbd") from exc
 
 
 @router.patch("/users")
