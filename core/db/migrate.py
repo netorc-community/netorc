a14f3ebe7c454a184c3fb99fc1acf91f656effda
Antonió <antonio@tomosev.dev>
2023-05-24 15:27:15 +0100

    added func return

--- core/db/migrate.py
+++ core/db/migrate.py
@@ -4,10 +4,11 @@ from core import db
 from core.metrics.logging import logger
 
 
-def migrate():
+def migrate() -> bool:
     try:
         SQLModel.metadata.create_all(db.engine)
         logger.info("Successfully migrated tables to database")
+        return True
 
     except Exception as exc:
         logger.error("Error migrating tables to database: %s", exc)
