2a3c05c542283fcc3ccc9dce38f26ae0a6517fe5
Antonió <antonio@tomosev.dev>
2023-05-26 17:58:22 +0100

    removed blank line

--- api/services/admin.py
+++ api/services/admin.py
@@ -71,7 +71,6 @@ def post_users(user_create: UserCreate):
     try:
         db_user = create_user(user)
         return db_user
-
     except Exception as exc:
         raise APIError(status_code=500, code="Post User", reason="Runtime error occurred") from exc
 
